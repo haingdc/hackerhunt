be708fe65f384096ccd4950eb33c3cb574ae8a6b
haingdc <haingdc@gmail.com>
2018-01-21 09:44:25 +0700

    Implement header

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,25 @@
 import raf from "./tempPolyfills";
-import Enzyme from "enzyme";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { expect } from "chai";
 
-import App from "./App";
+import App, { Header, Logo, Searchbar } from "./App";
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-describe("the environment", () => {
-  it("works, hopefully", () => {
-    expect(true).to.be.true;
+describe("App", () => {
+  it("should render Header", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<Header />)).be.equal(true);
+  });
+});
+
+describe("Header", () => {
+  it("should render Logo and Search bar", () => {
+    const wrapper = shallow(<Header />);
+    expect(
+      wrapper.containsAllMatchingElements([<Logo />, <Searchbar />]),
+    ).be.equal(true);
   });
 });

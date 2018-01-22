6fac6d4a3c7e129126e36ff9e22b3fb606b90491
haingdc <haingdc@gmail.com>
2018-01-23 01:15:18 +0700

    open source topic has children topic item

--- src/TopicList.js
+++ src/TopicList.js
@@ -4,11 +4,25 @@ class TopicList extends Component {
   render() {
     const { topics } = this.props;
     const liElems = topics.map(function(topic, index) {
+      let children;
+      if (topic.children) {
+        children = topic.children.map(function(child, i) {
+          return (
+            <div key={i} className="topic">
+              <a href="/">
+                <span className={`thumb ${child.className}`} />
+                {child.title}
+              </a>
+            </div>
+          );
+        });
+      }
       return (
         <div key={index} className="topic">
           <a href="/">
             <span className={`thumb ${topic.className}`} />
             {topic.title}
+            {children}
           </a>
         </div>
       );

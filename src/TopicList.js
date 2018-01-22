174956e57027b72287d047f95a6af25de75381ee
haingdc <haingdc@gmail.com>
2018-01-22 22:09:23 +0700

    each topic item has icon

--- src/TopicList.js
+++ src/TopicList.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-
+import "./TopicList.css";
 class TopicList extends Component {
   render() {
     const { topics } = this.props;
-    const liElems = topics.map(function(v, i) {
-      return <li key={i}>{v}</li>;
+    const liElems = topics.map(function(topic, index) {
+      return (
+        <li key={index}>
+          <span className={`thumb ${topic.className}`} />
+          {topic.title}
+        </li>
+      );
     });
     return (
       <aside>

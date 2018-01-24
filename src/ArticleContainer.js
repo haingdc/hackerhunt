2acf42f99b585da4d7ffc37ee686eafd0c89e138
haingdc <haingdc@gmail.com>
2018-01-24 23:22:07 +0700

    pass get articles

--- src/ArticleContainer.js
+++ src/ArticleContainer.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { articles } from "./data";
 
 class ArticleContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { articles: [] };
+    this.getArticles = this.getArticles.bind(this);
   }
+
+  getArticles() {
+    const data = [...articles["data"]];
+    this.setState({ articles: data });
+  }
+
   render() {
     return <div>ArticleContainer me here</div>;
   }
--- src/test/unit/ArticleContainer.test.js
+++ src/test/unit/ArticleContainer.test.js
@@ -6,6 +6,7 @@ import { expect } from "chai";
 import HeaderList from "../../components/HeaderList";
 import List from "../../List";
 import ArticleContainer from "../../ArticleContainer";
+import { articles } from "../../data";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,5 +14,11 @@ describe("ArticleContainer", () => {
   it("should start with empty list", () => {
     const wrapper = shallow(<ArticleContainer />);
     expect(wrapper.state("articles")).to.eql([]);
+  });
+
+  it("get articles", () => {
+    const wrapper = shallow(<ArticleContainer />);
+    wrapper.instance().getArticles();
+    expect(wrapper.state("articles")).to.eql(articles.data);
   });
 });

74bfeabf20d99158bf745e0fdbaf6c6ed8c13091
haingdc <haingdc@gmail.com>
2018-01-25 23:09:23 +0700

    pass "calls componentDidMount" in ArticleContainer.test.js

--- src/test/unit/ArticleContainer.test.js
+++ src/test/unit/ArticleContainer.test.js
@@ -3,6 +3,7 @@ import React from "react";
 import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { expect } from "chai";
+import sinon from "sinon";
 import ArticleContainer from "../../ArticleContainer";
 import Article from "../../components/Article";
 import { articles } from "../../data";
@@ -23,5 +24,13 @@ describe("ArticleContainer", () => {
     wrapper.update();
     expect(wrapper.state("articles")).to.eql(articles.data);
     expect(wrapper.find(Article)).to.have.length(16);
+  });
+
+  it("calls componentDidMount", () => {
+    sinon.spy(ArticleContainer.prototype, "componentDidMount");
+    const wrapper = mount(<ArticleContainer />);
+    expect(ArticleContainer.prototype.componentDidMount.calledOnce).to.equal(
+      true,
+    );
   });
 });

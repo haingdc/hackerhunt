1f9f6cfda70530894045a9336388e7991f847b52
haingdc <haingdc@gmail.com>
2018-01-27 01:11:03 +0700

    make up for vote and comment number at Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -27,9 +27,9 @@ const Article = props => {
         <span>
           <em>
             {/* prettier-ignore */}
-            <svg class="comment-12" width="12" height="12" xmlns="http://www.w3.org/2000/svg" data-reactid="88">
-              <g data-reactid="89">
-                <path d="M3.175 9.412c.842.375 1.803.588 2.825.588 3.314 0 6-2.239 6-5s-2.686-5-6-5-6 2.239-6 5c0 1.052.39 2.028 1.056 2.833l-1.056 3.167 3.175-1.588z" data-reactid="90" />
+            <svg class="comment-12" width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+              <g>
+                <path d="M3.175 9.412c.842.375 1.803.588 2.825.588 3.314 0 6-2.239 6-5s-2.686-5-6-5-6 2.239-6 5c0 1.052.39 2.028 1.056 2.833l-1.056 3.167 3.175-1.588z"/>
               </g>
             </svg>
           </em>
